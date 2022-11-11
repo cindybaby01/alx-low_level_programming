d44256d548d969f77f6144afb9304f100b7d6bf4
cindybaby01 <akinwalekehinde95@gmail.com>
2022-11-11 11:21:59 -0800

    Task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -31,10 +31,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		if (mem == NULL)
 			return (NULL);
 
-
 		return (mem);
 	}
-
 
 	if (new_size == 0 && ptr != NULL)
 	{
@@ -42,10 +40,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 
-
 	ptr_copy = ptr;
 	mem = malloc(sizeof(*ptr_copy) * new_size);
-
 
 	if (mem == NULL)
 	{
@@ -53,14 +49,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 
-
 	filler = mem;
-
-
 	for (index = 0; index < old_size && index < new_size; index++)
 		filler[index] = *ptr_copy++;
-
-
 	free(ptr);
 	return (mem);
 }
